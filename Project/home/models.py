e2758c70de814b4774f8515a85cfd07e810f21cb
Alejandro <84586035+AleHS-99@users.noreply.github.com>
2023-04-19 10:46:57 -0400

    Por aqui va la cosa
    Quitar algunas cosas agregar otras verse mejor

--- Project/home/models.py
+++ Project/home/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class modalidad(models.Model):
+    class Meta:
+        verbose_name = 'Modalidad'
+        verbose_name_plural = 'Modalidades'
+    codigo = models.AutoField(primary_key=True)
+    modal = models.CharField(max_length=100, verbose_name='Oferta')
+    def __str__(self):
+        return self.modal
+
+class oferta(models.Model):
+    class Meta:
+        verbose_name = "Ingrediente"
+        verbose_name_plural = "Ingredientes"
+    codigo = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=200)
+    foto = models.ImageField(upload_to='items',null=True,blank=True)
+    ingredientes = models.CharField(max_length=200,null=True,blank=True)
+    moda = models.ForeignKey(modalidad, on_delete=models.CASCADE,blank=False,null=False)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    def __str__(self):
+        return self.nombre
